9c4a09d03d62ecb82da6ce762b058a005956468b
Evgenii <emeleshkov@mail.ru>
2021-06-05 14:05:26 +0300

    done

--- Contents.swift
+++ Contents.swift
@@ -1,7 +1,20 @@
 import UIKit
-
+import Foundation
 var greeting = "Hello, playground"
 
 
 
 //if the string is palindrome
+//remuved whitespaces:))
+
+
+func checkIfPalindrome (input: String) -> Bool {
+    
+    let modifiedInput = input.replacingOccurrences(of: " ", with: "").lowercased()
+    return String(modifiedInput.reversed()) == input.replacingOccurrences(of: " ", with: "").lowercased()
+}
+
+checkIfPalindrome(input: "Top spot")            //true
+checkIfPalindrome(input: "Hello")               //false
+checkIfPalindrome(input: "No lemon, no melon")  //true
+//______________________________________________________________
